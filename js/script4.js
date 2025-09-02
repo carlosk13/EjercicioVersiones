e2f80584b611ae464ecd547b461386f178fb649f
Ivan <133200juan@gmail.com>
2025-09-02 13:31:34 -0600

    Ultimo revert

--- js/script4.js
+++ js/script4.js
@@ -1,3 +1,4 @@
 console.log("Nuevo comentario antes del commit para probar el revert")
-console.log("Cambnios")
-console.log("Nuevo comentario antes del commit para probar el revert")
+console.log("Nueva linea para hacer el revert")
+console.log("Nueva linea para hacer el revert")
+console.log("Nueva linea para hacer el revert")
